6ead5d63860e6e9949129869cf71a498bd907b0b
Megan Coyle <meg@mcoyle.com>
2016-07-27 09:32:11 -0400

    Complete Silver

--- app.rb
+++ app.rb
@@ -5,9 +5,46 @@ require 'active_record'
 
 require_relative 'db/connection'
 require_relative 'models/pokemon'
+require_relative 'models/trainer'
 
 get '/' do
   redirect '/pokemons'
+end
+
+get '/trainers' do
+  @trainers = Trainer.all
+  erb :"trainers/index"
+end
+
+get '/trainers/new' do
+  erb :"artists/new"
+end
+
+get '/trainers/:id' do
+  @trainer = Trainer.find(params[:id])
+  erb :"trainers/show"
+end
+
+post '/trainers' do
+  @trainer = Trainer.create(params[:trainer])
+  redirect "/trainers/#{@trainer.id}"
+end
+
+get '/trainers/:id/edit' do
+  @trainer = Trainer.find(params[:id])
+  erb :"artists/edit"
+end
+
+put '/trainers/:id' do
+  trainer = Trainer.find(params[:id])
+  trainer.update(params[:trainer])
+  redirect "/trainers/#{trainer.id}"
+end
+
+delete '/trainers/:id' do
+  trainer = Trainer.find(params[:id])
+  trainer.destroy
+  redirect '/trainers'
 end
 
 get '/pokemons' do
--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,18 @@
 DROP TABLE IF EXISTS pokemons;
+DROP TABLE IF EXISTS trainers;
+
+CREATE TABLE trainers (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  level TEXT NOT NULL,
+  img_url TEXT
+);
 
 CREATE TABLE pokemons (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   poke_type TEXT,
   cp INT NOT NULL,
-  img_url TEXT
+  img_url TEXT,
+  trainer_id INT
 );
